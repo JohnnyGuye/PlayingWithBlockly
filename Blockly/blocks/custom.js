8e56db30b3e911dfb39e0de6162d99d939641943
felixD50 <felix.dornier@insa-lyon.fr>
2016-06-29 17:03:24 +0200

    blocs core et generic sauf switch

--- Blockly/blocks/custom.js
+++ Blockly/blocks/custom.js
@@ -1,5 +1,6 @@
 goog.require('Blockly.Blocks');
 
+//blocks for testing
 Blockly.Blocks['decode_weft'] = {
     /*
     * This represents a weft with a size in bytes, a header,
@@ -74,5 +75,159 @@ Blockly.Blocks['simple_block'] = {
         this.setColour(290);
         this.setTooltip('');
         this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+//Generic : 
+
+Blockly.Blocks['decodebytes'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("DecodeBytes");
+        this.appendDummyInput()
+            .appendField("name :")
+            .appendField(new Blockly.FieldTextInput("default"), "name");
+        this.appendValueInput("start")
+            .setCheck(null)
+            .appendField("start byte position : ");
+        this.appendValueInput("end")
+            .setCheck(null)
+            .appendField("end byte position : ");
+        this.setInputsInline(false);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#e3vsv5');
+    }
+};
+
+Blockly.Blocks['decodeunsignedinteger'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Decode unsigned integer");
+        this.appendDummyInput()
+            .appendField("name : ")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME");
+        this.appendValueInput("leastSignificantBit")
+            .setCheck(null)
+            .appendField("least significant bit position");
+        this.appendValueInput("mostSignificantBit")
+            .setCheck(null)
+            .appendField("most significant bit position");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#4d9g5v');
+    }
+};
+
+Blockly.Blocks['decodesignedinteger'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Decode signed integer");
+        this.appendDummyInput()
+            .appendField("name : ")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME");
+        this.appendValueInput("leastSignificantBit")
+            .setCheck(null)
+            .appendField("least significant bit position :");
+        this.appendValueInput("mostSignificantBit")
+            .setCheck(null)
+            .appendField("most significant bit position :");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#yc64so');
+    }
+};
+
+Blockly.Blocks['compute'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Compute : ");
+        this.appendDummyInput()
+            .appendField("name : ")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME");
+        this.appendValueInput("function")
+            .setCheck(null)
+            .appendField("function :");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(330);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#skwmiv');
+    }
+};
+
+Blockly.Blocks['execute'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Execute ");
+        this.appendValueInput("action")
+            .setCheck(null)
+            .appendField("Action : ");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#iuhihs');
+    }
+};
+
+Blockly.Blocks['switch'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("switch");
+        this.appendDummyInput()
+            .appendField("variable :")
+            .appendField(new Blockly.FieldVariable("item"), "NAME");
+        this.appendValueInput("NAME")
+            .setCheck(null)
+            .appendField("case :")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME")
+            .appendField("then :");
+        this.appendValueInput("NAME")
+            .setCheck(null)
+            .appendField("case :")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME")
+            .appendField("then :");
+        this.appendValueInput("NAME")
+            .setCheck(null)
+            .appendField("case :")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME")
+            .appendField("then :");
+        this.appendValueInput("NAME")
+            .setCheck(null)
+            .appendField("case :")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME")
+            .appendField("then :");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#dsnuad');
+    }
+};
+
+//Core : 
+
+Blockly.Blocks['decodeboolean'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Decode boolean :");
+        this.appendDummyInput()
+            .appendField("name :")
+            .appendField(new Blockly.FieldTextInput("default"), "NAME");
+        this.appendValueInput("POSITION")
+            .setCheck(null)
+            .appendField("position :");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip('');
+        this.setHelpUrl('https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#re2b4h');
     }
 };
---
+++ Blockly/generators/csharp/custom.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+Blockly.CSharp.custom = {};
+
+Blockly.CSharp['decodebytes'] = function (block) {
+    var varName = block.getFieldValue('name');
+    var startPos = Blockly.CSharp.valueToCode(block, 'start', Blockly.CSharp.ORDER_ATOMIC);
+    var endPos = Blockly.CSharp.valueToCode(block, 'end', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.DecodeBytes("' + varName + '", ' + startPos + 'M, ' + endPos + 'M)';
+    return code;
+};
+
+Blockly.CSharp['decodeunsignedinteger'] = function (block) {
+    var varName = block.getFieldValue('NAME');
+    var leastsignificantbit = Blockly.CSharp.valueToCode(block, 'leastSignificantBit', Blockly.CSharp.ORDER_ATOMIC);
+    var mostsignificantbit = Blockly.CSharp.valueToCode(block, 'mostSignificantBit', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.DecodeUnsignedInteger("' + varName + '", ' + leastsignificantbit + 'M, ' + mostsignificantbit + 'M)';
+    return code;
+};
+
+Blockly.CSharp['decodesignedinteger'] = function (block) {
+    var varName = block.getFieldValue('NAME');
+    var leastsignificantbit = Blockly.CSharp.valueToCode(block, 'leastSignificantBit', Blockly.CSharp.ORDER_ATOMIC);
+    var mostsignificantbit = Blockly.CSharp.valueToCode(block, 'mostSignificantBit', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.DecodeSignedInteger("' + varName + '", ' + leastsignificantbit + 'M, ' + mostsignificantbit + 'M)';
+    return code;
+};
+
+Blockly.CSharp['compute'] = function (block) {
+    var varName = block.getFieldValue('NAME');
+    var expression = Blockly.CSharp.valueToCode(block, 'function', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.Compute(\n\t"' + varName + '",\n\tdecodingContextData => ' +expression+')';
+    return code;
+};
+
+Blockly.CSharp['execute'] = function (block) {
+    var action = Blockly.CSharp.valueToCode(block, 'action', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.Execute('+action+')';
+    return code;
+};
+
+Blockly.CSharp['decodeboolean'] = function (block) {
+    var varName = block.getFieldValue('NAME');
+    var position = Blockly.CSharp.valueToCode(block, 'POSITION', Blockly.CSharp.ORDER_ATOMIC);
+    var code = '.DecodeBoolean("' + varName + '", ' + position + 'M)';
+    return code;
+};
