ccb81e150ccaeeca2c067c5494ae8c2aef896d1a
Johnny <johnnyq@hotmail.fr>
2016-07-27 18:04:09 +0200

    Generator WIP

--- Generation/generation.viewmodel.js
+++ Generation/generation.viewmodel.js
@@ -1,6 +1,42 @@
 ﻿app.component("generator",
 {
     templateUrl: "Generation/generation.view.html",
-    controller: function() {}
+    controller: function () {
+        
+        var styleOpen = {
+            height: "100%",
+            transition: "0.4s",
+            overflow: "scroll"
+        };
+        var styleClosed = {
+            height: "0px",
+            transition: "0.4s",
+            overflow: "auto"
+        };
 
+        var full = {
+            height: "100%"
+        };
+        var halved = {
+            height: "70px"
+        };
+
+        this.open = false;
+        this.style = styleClosed;
+        this.full = halved;
+
+        this.Toggle = function() {
+            this.open = !this.open;
+            var gc = $("#generated-code");
+            var fullSize = '100%';
+            if (this.open) {
+                generateCode();
+                this.style = styleOpen;
+                this.full = full;
+            } else {
+                this.style = styleClosed;
+                this.full = halved;
+            }
+        };
+    }
 });
