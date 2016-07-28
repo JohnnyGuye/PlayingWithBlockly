0aa6bd0b183addfae39e765d82e1cdf1df28360a
Johnny <johnnyq@hotmail.fr>
2016-07-28 15:31:07 +0200

    A few scripts stays in place in the decoder page. But the rest is now in the place they should be.

---
+++ NotUsedCode.js
@@ -0,0 +1,47 @@
+﻿/**
+ * Create a new workspace for editing a function.
+ * @param {!Blockly.Workspace} workspace The workspace on which you want to add a function.
+ */
+function createFunction(workspace) {
+    var funcWindow = document.getElementById("functionWrapper");
+    if (funcWindow.style.display === "none") {
+        funcWindow.style.display = "block";
+    }
+
+    // Hide the previous workspace
+    if (nbWorkspaces > 0) {
+        document.getElementById(BASE_BLOCKLY_DIV + valueActivWorkspace).style.display = "none";
+    }
+
+    // Create anchor
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute("id", BASE_BLOCKLY_DIV + nbWorkspaces);
+    newDiv.setAttribute("class", BASE_BLOCKLY_DIV);
+    funcWindow.appendChild(newDiv);
+
+    // Create the workspace
+    var newWorkspace = Blockly.inject(BASE_BLOCKLY_DIV + nbWorkspaces,
+    { toolbox: document.getElementById('toolboxFunction') });
+    workspace.attachChildWorkspace(newWorkspace);
+    activWorkspace = newWorkspace;
+    valueActivWorkspace = nbWorkspaces;
+
+    // Create the onglet
+    var newOnglet = document.createElement('button');
+    newOnglet.setAttribute("class", "onglet");
+    newOnglet.setAttribute("name", "Nouveau");
+    newOnglet.setAttribute("id", "onglet" + nbWorkspaces);
+    newOnglet.setAttribute("value", nbWorkspaces);
+    newOnglet.setAttribute("checked", true);
+    newOnglet.setAttribute("onclick", "changeActiveWorkspace(" + nbWorkspaces + ")");
+    newOnglet.textContent = "Nouveau !";
+    document.getElementById("toolbar-onglet-func").appendChild(newOnglet);
+
+    nbWorkspaces++;
+}
+
+function changeActiveWorkspace(newValueActivWorkspace) {
+    document.getElementById(BASE_BLOCKLY_DIV + valueActivWorkspace).style.display = "none";
+    document.getElementById(BASE_BLOCKLY_DIV + newValueActivWorkspace).style.display = "block";
+    valueActivWorkspace = newValueActivWorkspace;
+} 
--- Workspace/workspace.viewmodel.js
+++ Workspace/workspace.viewmodel.js
@@ -1,12 +1,13 @@
 ﻿app.component("toolbox",
 {
-    templateUrl: "Toolbox/toolboxCoreGenerics.xml" 
+    templateUrl: "Toolbox/toolbox.xml" 
 });
 
 app.component("workspace",
     {
         templateUrl: "Workspace/workspace.view.html",
         controller: function () {
+
             workspace = Blockly.inject(BASE_BLOCKLY_DIV,
                     {
                         toolbox: $(princToolboxId)[0],
@@ -37,10 +38,102 @@ app.component("workspace",
 
             workspace.attachChildWorkspace(hiddenWorkspace);
             Squid.Storage.ReloadWorkspace(null, hiddenWorkspace);
-            //not optimal
-            //$(document).ready(Refresh);
-            setTimeout(Refresh, 250);
-            
+
+            this.Refresh = function() {
+                RefreshCategories(workspace, princToolboxId);
+                //for autocompletion
+                if (acTags) {
+                    acTags.matcher_ = new goog.ui.ac.ArrayMatcher(AutoComplete.GetTags(), true);
+                }
+            };
+
+            this.Save = function() {
+                Squid.Storage.SaveFunction(workspace);
+                Squid.Requests.SaveVariables(Squid.SimpleVariables);
+                this.Refresh();
+            };
+
+            this.Clear = function () {
+                function clearFunc(workspace_) {
+                    var children = workspace_.getLinkedWorkspace();
+                    for (var i = 0; i < children.length; i++) {
+                        children[i].clear();
+                        clearFunc(children[i]);
+                    }
+                    workspace_.clear();
+                }
+
+                if (window
+                    .confirm("Êtes vous sûr de vouloir vider tout l'espace de travail ?")) {
+                    clearFunc(workspace);
+                    Refresh();
+                }
+            }
+
+            this.Tags = "";
+            /**
+             * Searching with tags
+             * @param {} workspace 
+             * @param {} toolboxId 
+             * @returns {} 
+             */
+            this.SearchTag = function () {
+                //get all procedures defined in all existing workspaces
+                var toolboxId = princToolboxId;
+                var blocks = workspace.getAllDescendantBlocks();
+                var procedures = blocks.filter(function (item) {
+                    return item.getProcedureDef;
+                });
+
+                function parseTags(text) {
+                    var tags = text.replace(/\s+/g, '');
+                    return tags.split(",");
+                }
+                var searchWords = parseTags(this.Tags);
+                var count = 0;
+
+                // Get the results container tag and remove all the blocks it contains since the last research
+                var tagCategory = document.getElementById("SearchCategory");
+                while (tagCategory.firstChild) {
+                    tagCategory.removeChild(tagCategory.firstChild);
+                }
+
+                // For each block is all workspaces
+                for (var i = 0; i < procedures.length; i++) {
+
+                    var tags = procedures[i].getField("tags").text_;
+                    var procedureTags = parseTags(tags);
+
+                    //for each searched word 
+                    for (var j = 0; j < searchWords.length; j++) {
+                        for (var k = 0; k < procedureTags.length; k++) {
+                            if (searchWords[j] === procedureTags[k]) {
+                                count++;
+                                var newProcedure = document.createElement("block");
+                                if (procedures[i].getProcedureDef()[2]) {
+                                    newProcedure.setAttribute("type", "procedures_callreturn");
+                                } else {
+                                    newProcedure.setAttribute("type", "procedures_callnoreturn");
+                                }
+
+                                var mutator = document.createElement("mutation");
+                                mutator.setAttribute("name", procedures[i].getProcedureDef()[0]);
+
+                                newProcedure.appendChild(mutator);
+                                tagCategory.appendChild(newProcedure);
+                            }
+                        }
+                    }
+                }
+                // if no element has the tags requested :
+                if (count === 0) {
+                    alert("Aucun élément ne correspond à votre recherche.");
+                }
+                workspace.updateToolbox(document.getElementById(toolboxId));
+            }
+
+            //This method do not convince me. It seems not to work properly
+            setTimeout(this.Refresh, 1000);
 
             Squid.Dev.HideDevmode();
 
